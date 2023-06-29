2b42f0a0b916504070333ab61e2a9b0c40e35e78
Jeff <>
2023-06-29 15:23:48 +0800

    project setup

---
+++ ITunes Search/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ITunes Search
+//
+//  Created by Jeffrey Tabios on 6/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
